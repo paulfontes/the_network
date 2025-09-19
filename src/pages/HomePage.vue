aca4d587d2bf768a13840cde6031bb921a7f1a12
Paul Fontes <pgfon22@gmail.com>
2025-09-19 13:01:45 -0600

    started posttemplate

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,6 +4,7 @@ import { postsServices } from '@/services/PostsService.js';
 import { logger } from '@/utils/Logger.js';
 import { Pop } from '@/utils/Pop.js';
 import { computed, onMounted } from 'vue';
+import PostTemplate from './PostTemplate.vue';
 
 const posts = computed(() => AppState.posts)
 
@@ -28,7 +29,7 @@ async function getPosts() {
 <template>
   <div class="container">
     <section class="row">
-
+      <PostTemplate v-for="post in posts" :key="post.id" />
     </section>
   </div>
 </template>
---
+++ src/pages/PostTemplate.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+
+<template>
+    Post Template
+</template>
+
+
+<style lang="scss" scoped></style>
